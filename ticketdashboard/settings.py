69155e78252b6ab551cc2fc3722e8e53bb28da8e
Naveen Prakash <naveenprakashjk24@gmail.com>
2022-05-09 13:58:23 +0530

    heroku deploy

--- ticketdashboard/settings.py
+++ ticketdashboard/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-3&csj1-ta-+1=&mh&ev#*+=kgi92hkzg&_pgivur1uakhce8ut
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://ticketrequest.herokuapp.com','localhost','127.0.0.1']
+ALLOWED_HOSTS = ['https://service-dashboard-app.herokuapp.com','localhost','127.0.0.1']
 
 
 # Application definition
