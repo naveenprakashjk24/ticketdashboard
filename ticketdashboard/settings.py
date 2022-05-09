f441efc809ad8c238bee528582b9993447b2bd69
Naveen Prakash <naveenprakashjk24@gmail.com>
2022-05-09 14:04:21 +0530

    heroku deploy

--- ticketdashboard/settings.py
+++ ticketdashboard/settings.py
@@ -14,12 +14,11 @@ from pathlib import Path
 import os
 import django_heroku
 
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+print(BASE_DIR)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -131,7 +130,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    BASE_DIR / 'ticketapp/static'
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
